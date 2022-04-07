dc906c0cebc5cb5a486e2737f6146cec85122728
amRadhwen <radhweneamri9631@gmail.com>
2022-04-07 02:42:47 +0100

    ready

--- models/userModel.js
+++ models/userModel.js
@@ -48,7 +48,8 @@ const userSchema = new Schema({
     },
     role: {
         type: String,
-        required: true
+        required: true,
+        default: "client",
     },
     isAdmin: {
         type: Boolean,
